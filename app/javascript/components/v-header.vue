e82d145535c371abcc0b9d1e1f3282b79e1b6bab
Ivan Solovev <ivan@solovev.one>
2021-06-12 19:36:56 +0300

    feat: add header component

---
+++ app/javascript/components/v-header.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-navbar type="dark" variant="dark" class="mb-4">
+    <b-container fluid>
+      <b-navbar-brand>
+        <slot />
+      </b-navbar-brand>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+import { BNavbar, BContainer, BNavbarBrand } from "bootstrap-vue";
+
+
+export default {
+  name: "v-header",
+
+  components: {
+    BNavbar,
+    BContainer,
+    BNavbarBrand,
+  },
+};
+</script>
--- app/javascript/packs/vue.js
+++ app/javascript/packs/vue.js
@@ -4,11 +4,13 @@ import { BootstrapVue } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+import VHeader from "../components/v-header";
 import VCategory from "../components/v-category";
 import PageCategory from "../pages/page-category";
 
 Vue.use(BootstrapVue);
 
+Vue.component("v-header", VHeader);
 Vue.component("v-category", VCategory);
 Vue.component("page-category", PageCategory);
 
