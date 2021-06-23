ab98b8c859bb3450bfa6c0498e65b3abc9a7ab1d
Ivan Solovev <ivan@solovev.one>
2021-06-24 02:18:18 +0300

    refactor: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+main_category = Category.create({ title: 'Про Rails', description: 'Фреймворк, написанный на Ruby' })
+
+Post.create([{
+                title: 'Переписываем проект с Rails на Django',
+                author: 'Гарри Поттер',
+                image: 'django',
+                category_id: main_category.id
+             }, {
+               title: 'Выбрасываем из Rails лишнее',
+               content: 'В статье мы рассмотрим, как отпелить ненужные зависимости, например ActionMailer',
+               author: 'Воландеморт',
+               image: 'rails',
+               category_id: main_category.id
+             }
+            ])
+
+Category.create({ title: 'Про Django', description: 'Фреймворк для веб-приложений на Python' })
